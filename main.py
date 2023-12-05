7e98e11d9db4290ecb32155b1f1f374e25e382c6
Shams Ivon <shams919ivon@gmail.com>
2023-12-05 14:52:20 +0600

    make payment

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+my_coffee_maker = CoffeeMaker()
+my_menu = Menu()
+my_money_machine = MoneyMachine()
+
+# ordered_coffee = my_menu.find_drink("latte")
+# print(ordered_coffee.cost)
+
+while True:
+    order = input("​What would you like? (espresso/latte/cappuccino/): ")
+
+    if order == "off":
+        break   
+
+    if order == "report":
+        my_coffee_maker.report()
+        continue
+    
+    ordered_coffee = my_menu.find_drink(order)      
+
+    if ordered_coffee == None:
+        continue    
+
+    if my_coffee_maker.is_resource_sufficient(ordered_coffee) == False:
+        continue    
+
+    if my_money_machine.make_payment(ordered_coffee.cost) == True:
+        my_coffee_maker.make_coffee(ordered_coffee)
+
+
